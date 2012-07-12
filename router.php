68f50f6d3ea891a8ab42f40b08e4f26243459eb1
Elias da Rosa <elias@eliasdarosa.com.br>
2012-07-11 21:51:22 -0300

    Alterado as rotas

--- router.php
+++ router.php
@@ -1,42 +1,13 @@
 <?
 defined('BW') or die("Acesso negado!");
 
-function produtosBuildRoute(&$query)
-{
-	$segments = array();
+bwRouter::addUrl('/produtos');
+bwRouter::addUrl('/produtos/destaques');
+bwRouter::addUrl('/produtos/busca');
+bwRouter::addUrl('/produtos/detalhes/:metatagalias', '/produtos/detalhes', array(
+    ':metatagalias' => '[\w\d-]+'
+));
+bwRouter::addUrl('/produtos/categoria/:metatagalias', '/produtos/categoria', array(
+    ':metatagalias' => '[\w\d-]+'
+));
 
-	if(preg_match('#^categoria$|^detalhes$#', $query['view']))
-	{
-		$segments[] = $query['view'];
-		$segments[] = $query['id'];
-		$segments[] = $query['alias'];
-		unset($query['id'], $query['alias']);
-	}
-	
-	elseif(preg_match('#^destaques$|^busca$#', $query['view']))
-	{
-		$segments[] = $query['view'];
-	}
-	
-	return $segments;
-}
-
-function produtosParseRoute($segments)
-{
-	$vars = array();
-	
-	if(preg_match('#^categoria$|^detalhes$#', $segments[0]) && count($segments) == 3)
-	{
-		$vars['view'] = $segments[0];
-		$vars['id'] = $segments[1];
-		$vars['alias'] = $segments[2];
-	}
-	
-	elseif(preg_match('#^destaques$|^busca$#', $segments[0]) && count($segments) == 1)
-	{
-		$vars['view'] = $segments[0];
-	}
-		
-	return $vars;	
-}
-?>
