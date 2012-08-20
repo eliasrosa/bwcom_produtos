9837a38f4485acd4411c779ff7925cd44d1d809a
Elias da Rosa <elias@eliasdarosa.com.br>
2012-08-20 17:15:27 -0300

    Atualizado as rotas

--- router.php
+++ router.php
@@ -1,16 +1,28 @@
 <?
+
 defined('BW') or die("Acesso negado!");
 
-bwRouter::addUrl('/produtos');
-bwRouter::addUrl('/produtos/destaques', array());
-bwRouter::addUrl('/produtos/busca');
-bwRouter::addUrl('/produtos/detalhes/:alias/:id', array('nome', 'id'));
-bwRouter::addUrl('/produtos/categoria/:alias/:id', array('nome', 'id'));
+bwRouter::addUrl('/produtos/destaques', array(
+    'title' => 'Produtos em destaque'
+));
+
+bwRouter::addUrl('/produtos/busca', array(
+    'title' => 'Buscar produtos'
+));
+
+bwRouter::addUrl('/produtos/detalhes/:alias/:id', array(
+    'fields' => array('nome', 'id')
+));
+
+bwRouter::addUrl('/produtos/categoria/:alias/:id', array(
+    'fields' => array('nome', 'id')
+));
 
 
 // ADM
-bwRouter::addUrl('/produtos/task', array(), 'task');
+bwRouter::addUrl('/produtos');
+bwRouter::addUrl('/produtos/task', array('type' => 'task'));
 bwRouter::addUrl('/produtos/lista');
-bwRouter::addUrl('/produtos/cadastro/:id', array('id'));
+bwRouter::addUrl('/produtos/cadastro/:id', array('fields' => array('id')));
 bwRouter::addUrl('/produtos/categorias/lista');
-bwRouter::addUrl('/produtos/categorias/cadastro/:id', array('id'));
+bwRouter::addUrl('/produtos/categorias/cadastro/:id', array('fields' => array('id')));
